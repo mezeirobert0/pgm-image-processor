3e60e5a8d3170dd105610a98bfc64a3fcbe46ee9
mezeirobert0 <antoniu.mezei@stud.ubbcluj.ro>
2024-04-03 12:49:39 +0300

    Manually tested the functions in Image class

--- pgm-image-processor/app.cpp
+++ pgm-image-processor/app.cpp
@@ -5,10 +5,19 @@ int main()
 {
     Image img;
     img.load("../images/small_image.ascii.pgm");
+    img.save("../images/small_image.ascii.pgm");
+
     Image newImg;
     img.getROI(newImg, 1, 1, 2, 2);
-    img.save("../images/small_image.ascii.pgm");
-    newImg.save("../images/small_image_roi.ascii.pgm");
+
+    Image newImg2;
+    img.getROI(newImg2, 0, 0, 2, 2);
+    
+    std::cout << newImg2 << "\n\n" << newImg << "\n\n" << newImg2 - newImg << "\n\n";
+
+    std::cout << (int)(img.at(2, 1)) << "\n\n";
+
+    std::cout << Image::zeros(4, 4);
 
     return 0;
 }
